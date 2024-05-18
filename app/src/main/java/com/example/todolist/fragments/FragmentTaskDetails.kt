eb51434ac347b42908b6358b2a2cb891e8cd6280
m4tiss <mateusz039@op.pl>
2024-05-18 17:12:21 +0200

    display photo in task

--- app/src/main/java/com/example/todolist/fragments/FragmentTaskDetails.kt
+++ app/src/main/java/com/example/todolist/fragments/FragmentTaskDetails.kt
@@ -2,14 +2,17 @@ package com.example.todolist.fragments
 
 import android.annotation.SuppressLint
 import android.app.ActivityManager.TaskDescription
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.todolist.MainActivity
 import com.example.todolist.R
 import com.example.todolist.database.TaskModel
@@ -25,6 +28,7 @@ class FragmentTaskDetails(private val task: TaskModel, private var tasksViewMode
     private lateinit var taskCategory: TextView
     private lateinit var completedImageView: ImageView
     private lateinit var notificationSwitch: SwitchCompat
+    private lateinit var taskAttachments: LinearLayout
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -35,6 +39,7 @@ class FragmentTaskDetails(private val task: TaskModel, private var tasksViewMode
         val view = inflater.inflate(R.layout.fragment_details, container, false)
 
 
+        taskAttachments = view.findViewById(R.id.taskAttachments)
         taskTitle = view.findViewById(R.id.titleTextView)
         taskDescription = view.findViewById(R.id.descriptionTextView)
         executionTime = view.findViewById(R.id.executionTimeTextView)
@@ -56,6 +61,7 @@ class FragmentTaskDetails(private val task: TaskModel, private var tasksViewMode
         }
         notificationSwitch.isChecked = task.notificationEnabled == 1
         println(task.completed)
+        displayAttachments()
 
         return view
     }
@@ -70,6 +76,27 @@ class FragmentTaskDetails(private val task: TaskModel, private var tasksViewMode
 //            }
 //        }
 
+    }
+    private fun displayAttachments() {
+        val attachments = task.attachments
+
+        if (attachments.isNotEmpty()) {
+            for (attachment in attachments) {
+                val imageView = ImageView(context)
+                val layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                imageView.layoutParams = layoutParams
+                imageView.setPadding(0, 2, 0, 2)
+
+                Glide.with(this)
+                    .load(Uri.parse(attachment))
+                    .into(imageView)
+
+                taskAttachments.addView(imageView)
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/todolist/viewModels/TasksViewModel.kt
+++ app/src/main/java/com/example/todolist/viewModels/TasksViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.todolist.viewModels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.todolist.database.TaskModel
